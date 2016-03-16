0948c85be9f0fc419496aec583f7720277980239
chenyufeng <503434355@qq.com>
2016-03-17 00:22:18 +0800

    Initial Commit

---
+++ Block_WeakSelf/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Block_WeakSelf
+//
+//  Created by chenyufeng on 16/3/17.
+//  Copyright © 2016年 Netease. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Block_WeakSelf/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Block_WeakSelf
+//
+//  Created by chenyufeng on 16/3/17.
+//  Copyright © 2016年 Netease. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
